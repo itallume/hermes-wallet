5a61e336ec9c1372295057b52bbb61113750b074
itallume <itallo.oliver21@gmail.com>
2025-04-29 22:27:17 -0300

    first commit

---
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/HermesWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.pweb2.HermesWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpb/pweb2/HermesWallet/HermesWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.pweb2.HermesWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
