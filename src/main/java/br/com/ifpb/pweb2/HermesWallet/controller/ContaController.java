1696fc95bdd439318c5dd67a8123847056a76e02
steph4nn <laurostephan@gmail.com>
2025-05-23 00:42:54 -0300

    feat: view conta funcionando parcialmente

--- src/main/java/br/com/ifpb/pweb2/HermesWallet/controller/ContaController.java
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/controller/ContaController.java
@@ -1,19 +1,43 @@
 package br.com.ifpb.pweb2.HermesWallet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpServletRequest;
+import br.com.ifpb.pweb2.HermesWallet.models.Conta;
+import br.com.ifpb.pweb2.HermesWallet.models.TipoConta;
+import br.com.ifpb.pweb2.HermesWallet.service.ContaService;
+
 
 @RequestMapping("/conta")
 @Controller
 public class ContaController {
 
-    @RequestMapping("/main")
-    public String contaHomePage(HttpServletRequest request, Model model){
-        return "conta";
+    @Autowired
+    ContaService _contaService;
+
+    @GetMapping("create")
+    public ModelAndView createForm(ModelAndView model) {
+        model.addObject("conta", new Conta());
+        model.addObject("tiposConta", TipoConta.values());
+        model.setViewName("conta/formulario");
+        return model;
+    }
+    
+    @GetMapping("list")
+    public ModelAndView list( ModelAndView model) {
+        model.addObject("contas", _contaService.getContas());
+        model.setViewName("conta/lista");
+        return model;
     }
 
-    
+    @PostMapping("save")
+    public ModelAndView save(Conta conta, ModelAndView model) {
+        model.addObject("correntista", _contaService.createConta(conta).getCorrentista());
+        model.setViewName("conta/lista");
+        return model;
+    }
 }
--- src/main/java/br/com/ifpb/pweb2/HermesWallet/service/ContaService.java
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/service/ContaService.java
@@ -1,26 +1,45 @@
 package br.com.ifpb.pweb2.HermesWallet.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.ifpb.pweb2.HermesWallet.models.Conta;
 import br.com.ifpb.pweb2.HermesWallet.models.Correntista;
 import br.com.ifpb.pweb2.HermesWallet.repository.ContaRepository;
+import br.com.ifpb.pweb2.HermesWallet.repository.CorrentistaRepository;
 import jakarta.transaction.Transactional;
 
+@Service
 public class ContaService {
-        @Autowired
-    private ContaRepository _accountRepository;
+    @Autowired
+    private ContaRepository _contaRepository;
     
+    @Autowired
+    private CorrentistaRepository _correntistaRepository;
+
+
+    //TODO remover gambiarra do correntista
     @Transactional
-    public Conta createAccount(Conta newAccount){
-        return _accountRepository.save(newAccount);
-    } 
+    public Conta createConta(Conta contaNova){
+        Optional<Correntista> c = _correntistaRepository.findById(1233L);
+        if(c.isPresent()){
+            contaNova.setCorrentista(c.get());
+        }
+        return _contaRepository.save(contaNova);
+    }
 
     @Transactional
-    public List<Conta> getAccountsByAccountHolder(Correntista user) {
-        List<Conta> accounts = _accountRepository.findByAccountHolderId(user.getId());
+    public List<Conta> getContas() {
+        List<Conta> accounts = _contaRepository.findAll();
+        return accounts;
+    }
+
+    @Transactional
+    public List<Conta> getContasByCorrentista(Correntista user) {
+        List<Conta> accounts = _contaRepository.findByCorrentistaId(user.getId());
         return accounts;
     }
 
