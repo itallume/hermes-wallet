b5a577dc51fc8914483b885263abdc5a9ea6cb41
pauloabaia <paulinhus2@outlook.com>
2025-05-24 18:13:29 -0300

    Controller & View de transacao

--- src/main/java/br/com/ifpb/pweb2/HermesWallet/controller/TransacaoController.java
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/controller/TransacaoController.java
@@ -1,12 +1,56 @@
 package br.com.ifpb.pweb2.HermesWallet.controller;
 
+import br.com.ifpb.pweb2.HermesWallet.models.Conta;
+import br.com.ifpb.pweb2.HermesWallet.models.Transacao;
+import br.com.ifpb.pweb2.HermesWallet.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Controller
+@RequestMapping("conta/{idConta}/transacao") //conta?id?/trans
 public class TransacaoController {
 
-//    @Controller
-//    @RequestMapping("/transacao") //conta?id?/trans
+    @Autowired
+    TransacaoService transacaoService;
 
+    @GetMapping("/form")
+    public ModelAndView getForm(Transacao transacao, ModelAndView modelAndView, RedirectAttributes attr){
+        if (transacao.getConta() == null) {
+            transacao.setConta(new Conta());
+        }
+        modelAndView.addObject("transacao", transacao);
+        modelAndView.setViewName("transacao/formularioTransacao");
+        return modelAndView;
+    }
+
+    @PostMapping()
+    public ModelAndView save(@PathVariable( value = "idConta") Long id, Transacao transacao, ModelAndView modelAndView, RedirectAttributes attr){
+        transacaoService.createTransacao(transacao);
+        attr.addFlashAttribute("mensagem", "Transacao criada com sucesso!");
+        modelAndView.setViewName("redirect:/conta/" + id + "/transacao/list");
+        return modelAndView;
+    }
+
+    @GetMapping("/list")
+    public ModelAndView listAll(@PathVariable( value = "idConta") Long id, ModelAndView modelAndView){
+        modelAndView.addObject("transacao", transacaoService.findAllById(id));
+        modelAndView.setViewName("transacao/list");
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView getTransacao(@PathVariable( value = "idConta") Long idConta,
+                                     @PathVariable("id") Long id,//id transacao
+                                     ModelAndView modelAndView){
+        modelAndView.addObject("transacao", transacaoService.findById(id));
+        modelAndView.setViewName("transacao/formularioTransacao");
+        return modelAndView;
+    }
 
 }
--- src/main/java/br/com/ifpb/pweb2/HermesWallet/service/TransacaoService.java
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/service/TransacaoService.java
@@ -5,11 +5,13 @@ import br.com.ifpb.pweb2.HermesWallet.repository.ContaRepository;
 import br.com.ifpb.pweb2.HermesWallet.repository.TransacaoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TransacaoService {
 
     @Autowired
@@ -22,6 +24,13 @@ public class TransacaoService {
     public Transacao createTransacao(Transacao novaTransacao){
         return _transacaoRepository.save(novaTransacao);
     }
+
+    @Transactional
+    public Transacao editTransacao(Long id,Transacao transacaoAtt){ //existe null
+        transacaoAtt.setId(id);
+        return _transacaoRepository.save(transacaoAtt);
+    }
+
     @Transactional
     public Optional<Transacao> findById(Long contaId){ //se retornar null?
         return _transacaoRepository.findById(contaId);
