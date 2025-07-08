90d2d0507d4009c0541977f842c68430174faa2b
itallume <itallo.oliver21@gmail.com>
2025-07-08 12:04:48 -0300

    bug: bug na aplicação do layout na tela de cadastro correntista

--- src/main/java/br/com/ifpb/pweb2/HermesWallet/controller/CorrentistaController.java
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/controller/CorrentistaController.java
@@ -1,9 +1,43 @@
 package br.com.ifpb.pweb2.HermesWallet.controller;
 
+import br.com.ifpb.pweb2.HermesWallet.models.Correntista;
+import br.com.ifpb.pweb2.HermesWallet.service.CorrentistaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Controller
+@RequestMapping("/correntista")
 public class CorrentistaController {
+    @Autowired
+    private CorrentistaService correntistaService;
 
+    @GetMapping("/form")
+    public ModelAndView getForm(Correntista correntista, ModelAndView model){
+        model.addObject("correntista", correntista);
+        model.setViewName("correntista/cadastroCorrentista");
+        return model;
+    }
+
+    @PostMapping
+    public ModelAndView save(Correntista correntista, ModelAndView model, RedirectAttributes attr){
+        //TODO regras de negocio
+        correntistaService.save(correntista);
+        attr.addFlashAttribute("mensagem", "Correntista inserido com sucesso!");
+        model.setViewName("redirect:/correntista");
+        return model;
+    }
+
+    @GetMapping
+    public ModelAndView list(ModelAndView model){
+        model.addObject("correntistas", correntistaService.findAll());
+        model.setViewName("correntista/listagemCorrentistas");
+        return model;
+    }
 
 }
