61d0a05dcb86053347dabbb73e45e6e0256eff7d
Joao Paulo Azevedo Baia <20231370018@ad.ifpb.edu.br>
2025-07-29 18:30:57 -0300

    feat: nova validação de erros

--- src/main/java/br/com/ifpb/pweb2/HermesWallet/models/Conta.java
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/models/Conta.java
@@ -1,10 +1,16 @@
 package br.com.ifpb.pweb2.HermesWallet.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
+import org.springframework.context.MessageSource;
 import java.time.LocalDate;
 import java.util.List;
+
 
 @Data
 @Entity
@@ -13,18 +19,27 @@ public class Conta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "{erro.blank}")
     private String descricao;
+
+    @NotBlank(message = "{erro.blank}")
     private String numero;
-    private LocalDate diaFechamento;
+
+    @NotNull(message = "{erro.null}")
+    @Min(value = 1, message = "{erro.data.min}")
+    @Max(value=31, message = "{erro.data.max}")
+    private Integer diaFechamento;
 
     @Enumerated(EnumType.STRING)
     private TipoConta tipo;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "correntista_id")  // explicita a coluna FK
+    @Valid
+    @JoinColumn(name = "correntista_id")
     private Correntista correntista;
 
+    @Valid
     @OneToMany (mappedBy = "conta", cascade = CascadeType.ALL)
     private List<Transacao> transaction;
-    //TODO correntistas
 }
--- src/main/java/br/com/ifpb/pweb2/HermesWallet/models/Correntista.java
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/models/Correntista.java
@@ -1,9 +1,15 @@
 package br.com.ifpb.pweb2.HermesWallet.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.List;
 
@@ -16,11 +22,25 @@ public class Correntista {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "{erro.blank}")
+    @Size(min = 2, max = 128)
     private String nome;
+
+    @CPF
+    @NotBlank(message = "{erro.blank}")
     private String cpf;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "{erro.blank}")
+    @Size(min = 6, max = 128)
     private String senha;
+
+    @NotNull(message = "{erro.null}")
     private boolean admin;
+
     @OneToMany(mappedBy = "correntista", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Conta> contas;
 }
