747942487f4e2a6f7457b35efea27a3208df401d
Fernando Julio <julio8martins@gmail.com>
2025-08-24 23:14:04 -0300

    feat: service categoria & categoriaNaoEncontradaException

---
+++ src/main/java/br/com/ifpb/pweb2/HermesWallet/exceptions/CategoriaNaoEncontradaException.java
@@ -0,0 +1,6 @@
+package br.com.ifpb.pweb2.HermesWallet.exceptions;
+
+public class CategoriaNaoEncontradaException extends Exception {
+    public CategoriaNaoEncontradaException(String message){ super(message);}
+}
+
